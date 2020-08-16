3aaec69bd9d92e7d801ebe3468e6cd5be9291940
Mónika Vaszilykó <vmonika028@gmail.com>
2020-08-16 13:35:57 +0200

    lots of updates

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const menuButton = document.querySelector('.menu-button')
 const menu = document.querySelector('.menu')
 const dates = document.getElementsByClassName('date')
+const backToTopButton = document.getElementById('back-to-top')
 
-menuButton.addEventListener('click', (e) => {
+menuButton.addEventListener('click', () => {
     menu.classList.toggle('visible')
 })
 
@@ -10,3 +11,7 @@ for (const input of dates) {
     input.addEventListener('focusin', (e) => e.target.type = 'date')
     input.addEventListener('focusout', (e) => e.target.type = 'text')
 }
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo(0, 0)
+})
